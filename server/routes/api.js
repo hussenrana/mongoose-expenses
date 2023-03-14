8289bafbffdaa384f8e1f5df06ba3e65e58360d8
Rana Hussein <hussenrana3@gmail.com>
2023-03-14 14:40:41 +0200

    update project

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,11 +5,64 @@ const app = express();
 const Expense = require("../model/Expense");
 
 router.get("/expenses", function (req, res) {
-  Expense.find({})
-    .sort({ date: "asc" })
-    .then(function (expenses) {
+  const d1 = req.query.d1;
+  const d2 = req.query.d2
+    ? req.query.d2
+    : moment(req.query.d2).format("YYYY-MM-DD");
+  if (d1) {
+    Expense.find({
+      $and: [{ date: { $gt: d1 } }, { date: { $lt: d2 } }],
+    })
+      .sort({ date: -1 })
+      .then(function (expenses) {
+        res.send(expenses);
+      });
+  }
+
+  //   if (d1 || d2) {
+  //     const dToday = d1 ? d1 : d2;
+  //     const d = moment(dToday).format("LLLL");
+  //     Expense.find({
+  //       date: { $gt: d },
+  //     })
+  //       .sort({ date: -1 })
+  //       .then(function (expenses) {
+  //         res.send(expenses);
+  //       });
+  //   }
+  else {
+    Expense.find({})
+      .sort({ date: -1 })
+      .then(function (expenses) {
+        res.send(expenses);
+      });
+  }
+});
+
+router.get("/expenses/:group", function (req, res) {
+  const total = req.query.total;
+  const group = req.params.group;
+  if (total === "true") {
+    Expense.aggregate([
+      {
+        $match: { group: group },
+      },
+      {
+        $group: {
+          _id: "$group",
+          totalAmount: { $sum: "$amount" },
+        },
+      },
+    ]).then(function (totalAmount) {
+      res.send(totalAmount);
+    });
+  } else {
+    Expense.find({
+      group: group,
+    }).then(function (expenses) {
       res.send(expenses);
     });
+  }
 });
 
 router.post("/expense", function (req, res) {
@@ -36,4 +89,5 @@ router.put("/update/:group1/:group2", function (req, res) {
       res.status(500).send(err);
     });
 });
+
 module.exports = router;
